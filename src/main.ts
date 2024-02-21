c225fcb49dd7c2ff1781ec07be4f594e99d3307e
Mattakushi1337 <maksim1337.ru@mail.ru>
2024-02-21 15:08:34 +0700

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import * as process from 'process';
 
 async function bootstrap() {
-
-
   const app = await NestFactory.create(AppModule);
   app.use(cors({ origin: '*' }));
 
-  await app.listen(3001);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 
 bootstrap();
