a2f4cea8506f81631277a0874c49b9d87aed59d7
Mattakushi1337 <maksim1337.ru@mail.ru>
2024-02-21 14:59:26 +0700

    try to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
 import * as cors from 'cors';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync('./src/cert/key.pem'),
-    cert: fs.readFileSync('./src/cert/cert.pem'),
-  };
 
-  const app = await NestFactory.create(
-    AppModule,
-    { httpsOptions },
-  );
+
+  const app = await NestFactory.create(AppModule);
   app.use(cors({ origin: '*' }));
 
   await app.listen(3001);
