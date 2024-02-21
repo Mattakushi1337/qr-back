ab867fe5cb937cab5b1937e04fd4246fbb58d717
Mattakushi1337 <maksim1337.ru@mail.ru>
2024-02-21 15:50:27 +0700

    3 commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
 import { IntdataModule } from './intdata/intdata.module';
-import { IntDataController } from './intdata/intdata.controller';
-import { IntDataService } from './intdata/intdata.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'HelloWorld2023!',
-      database: 'qr',
+      host: process.env.DATABASE_HOST,
+      port: +process.env.DATABASE_PORT,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DBNAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
     }),
     DataModule,
