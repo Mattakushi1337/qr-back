abc6b3891b374f22362815f65aee5b68e3da3bfe
Mattakushi1337 <maksim1337.ru@mail.ru>
2024-05-17 15:24:14 +0700

    update cors2

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: "*",
-    allowedHeaders: "*",
-    credentials: true,
+
+  app.use(cors({ origin: '*' }));
+
+  const httpServer = app.getHttpServer();
+  const io = require('socket.io')(httpServer, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
   });
 
   await app.listen(parseInt(process.env.PORT));
