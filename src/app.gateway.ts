f3006a6bbac1214845ec55ebad851c43116fb503
Mattakushi1337 <maksim1337.ru@mail.ru>
2024-05-17 16:53:34 +0700

    update cors5

--- src/app.gateway.ts
+++ src/app.gateway.ts
@@ -1,7 +1,14 @@
 import { WebSocketGateway, WebSocketServer, OnGatewayInit, SubscribeMessage, MessageBody } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
+@WebSocketGateway({
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        allowedHeaders: '*',
+        credentials: true
+    }
+})
 export class AppGateway implements OnGatewayInit {
     @WebSocketServer() server: Server;
 
@@ -20,28 +27,24 @@ export class AppGateway implements OnGatewayInit {
     @SubscribeMessage('animation_1')
     handleAnimation1(@MessageBody() data: any): void {
         console.log('animation_1', data);
-
         this.server.emit('animation_1', data);
     }
 
     @SubscribeMessage('animation_2')
     handleAnimation2(@MessageBody() data: any): void {
         console.log('animation_2', data);
-
         this.server.emit('animation_2', data);
     }
 
     @SubscribeMessage('animation_3')
     handleAnimation3(@MessageBody() data: any): void {
         console.log('animation_3', data);
-
         this.server.emit('animation_3', data);
     }
 
     @SubscribeMessage('animation_4')
     handleAnimation4(@MessageBody() data: any): void {
         console.log('animation_4', data);
-
         this.server.emit('animation_4', data);
     }
 }
