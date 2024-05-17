67ef561e5464fcb0a7719168d09a140057e79c47
Mattakushi1337 <maksim1337.ru@mail.ru>
2024-05-17 10:56:22 +0700

    add cors for webView

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 import * as cors from 'cors';
 
 async function bootstrap() {
@@ -7,6 +8,15 @@ async function bootstrap() {
   app.use(cors({ origin: '*' }));
 
   await app.listen(parseInt(process.env.PORT));
+  const io = require('socket.io')(await app.listen(parseInt(process.env.PORT)), {
+    cors: {
+      origin: '*',
+    },
+  });
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+
+  await app.init();
 }
 
 bootstrap();
