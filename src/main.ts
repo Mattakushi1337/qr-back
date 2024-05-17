02988fca8e60641c96837ee0816b6b085ef71892
Mattakushi1337 <maksim1337.ru@mail.ru>
2024-05-17 15:02:12 +0700

    add cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors({ origin: '*' }));
+  app.enableCors({
+    origin: '*', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
 
-  await app.listen(parseInt(process.env.PORT));
+  await app.listen(parseInt(process.env.PORT) );
 }
 
 bootstrap();
