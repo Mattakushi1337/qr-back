4cfa41b81e7c5956aa9bb66e475cc66f01f2e5a2
Mattakushi1337 <maksim1337.ru@mail.ru>
2024-05-17 11:03:03 +0700

    delete cors

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { IoAdapter } from '@nestjs/platform-socket.io';
 import * as cors from 'cors';
 
 async function bootstrap() {
@@ -8,15 +7,6 @@ async function bootstrap() {
   app.use(cors({ origin: '*' }));
 
   await app.listen(parseInt(process.env.PORT));
-  const io = require('socket.io')(await app.listen(parseInt(process.env.PORT)), {
-    cors: {
-      origin: '*',
-    },
-  });
-
-  app.useWebSocketAdapter(new IoAdapter(app));
-
-  await app.init();
 }
 
 bootstrap();
